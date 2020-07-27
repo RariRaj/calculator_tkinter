fca2b0c46abd3cef0ca5bfa9f8e4b1636ea6cd0f
Rari Raj <rariraj95@gmail.com>
2020-07-27 21:34:41 +0530

    all scientific mode operations are done

--- my_calculator.py
+++ my_calculator.py
@@ -59,6 +59,7 @@ entry_field.pack(side=TOP, ipadx=20)
 
 # Place a frame to include all buttons
 frame = Frame(window)
+frame.config(bg="powder blue")
 frame.pack(side=TOP)
 
 # for Button widgets
@@ -154,6 +155,7 @@ btn_equal.grid(row=7, column=4, columnspan=2)
 # /////////////////////////////////////////////////////////////////////////////////////////////
 
 # code for Scientific Mode
+# PLEASE NOTE THAT i have designed scientific mode which first accept numbers and then the operation
 
 
 normal_clicked = True
@@ -183,94 +185,96 @@ def sc_calc(event):
     sc_text = sb['text']
     exp = entryText.get()
     if sc_text == "sinθ":
-        sc_val = str(m.sin(m.radians(int((exp)))))
-        entryText.set("")
+        sc_val = str(m.sin(m.radians(int(exp))))
         entryText.set(sc_val)
 
     elif sc_text == "cosθ":
-        sc_val = str(m.cos(m.radians(int((exp)))))
-        entryText.set("")
+        sc_val = str(m.cos(m.radians(int(exp))))
         entryText.set(sc_val)
 
     elif sc_text == "tanθ":
-        sc_val = str(m.tan(m.radians(int((exp)))))
-        entryText.set("")
+        sc_val = str(m.tan(m.radians(int(exp))))
         entryText.set(sc_val)
 
     elif sc_text == "x!":
         sc_val = str(m.factorial(int(exp)))
-        entryText.set("")
         entryText.set(sc_val)
 
     elif sc_text == "^":
         base, power = exp.split(",")
         sc_val = str(m.pow(int(base), int(power)))
-        entryText.set("")
         entryText.set(sc_val)
 
     elif sc_text == "ToDeg":
         sc_val = str(m.degrees(float(exp)))
-        entryText.set("")
         entryText.set(sc_val)
 
     elif sc_text == "ToRad":
         sc_val = str(m.radians(float(exp)))
-        entryText.set("")
         entryText.set(sc_val)
 
     elif sc_text == "√":
         sc_val = str(m.sqrt(int(exp)))
-        entryText.set("")
         entryText.set(sc_val)
 
-    elif sc_text==",":
-        entryText.set(exp+sc_text)
+    elif sc_text == ",":
+        entryText.set(exp + sc_text)
+
+    elif sc_text == "log":
+        x, base = exp.split(",")
+        sc_val = str(m.log(int(x), int(base)))
+        entryText.set(sc_val)
+
+    elif sc_text == "∏":
+        sc_val = str(m.pi * (int(exp)))
+        entryText.set(sc_val)
+
+    elif sc_text == "└ Floor┘":
+        sc_val = str(m.floor(float(exp)))
+        entryText.set(sc_val)
 
 
-
-
-# menu bar to show scientic mode
-menubar = Menu(window)
-mode = Menu(menubar, tearoff=0)
+# menu bar to show scientific mode
+menuBar = Menu(window, activebackground='orange', foreground="red", font="Times 12 ")
+mode = Menu(menuBar, tearoff=0)
 mode.add_checkbutton(label="Scientific Calculator", command=show_btn)
-menubar.add_cascade(label="Menu", menu=mode)
-window.config(menu=menubar)
+menuBar.add_cascade(label="Menu", menu=mode)
+window.config(menu=menuBar)
 
-# create a frame for scientic mode buttons
+# create a frame for scientific mode buttons
 sc_frame = Frame(window)
+sc_frame.config(bg="powder blue")
 
 # scientific mode buttons (action performed without using lambda function)
 
 
-sin_btn = Button(sc_frame, text="sinθ", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-sin_btn.grid(row=0, column=0)
-cos_btn = Button(sc_frame, text="cosθ", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-cos_btn.grid(row=0, column=1)
-tan_btn = Button(sc_frame, text="tanθ", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-tan_btn.grid(row=0, column=2)
-fact_btn = Button(sc_frame, text="x!", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-fact_btn.grid(row=0, column=3)
+sin_btn = Button(sc_frame, text="sinθ", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+sin_btn.grid(row=0, column=0, padx=4)
+cos_btn = Button(sc_frame, text="cosθ", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+cos_btn.grid(row=0, column=1, padx=4)
+tan_btn = Button(sc_frame, text="tanθ", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+tan_btn.grid(row=0, column=2, padx=4)
+fact_btn = Button(sc_frame, text="x!", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+fact_btn.grid(row=0, column=3, padx=4)
 
+pow_btn = Button(sc_frame, text="^", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+pow_btn.grid(row=0, column=4, padx=4)
+deg_btn = Button(sc_frame, text="ToDeg", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+deg_btn.grid(row=0, column=5, padx=4)
+rad_btn = Button(sc_frame, text="ToRad", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+rad_btn.grid(row=1, column=0, padx=4)
+sqr_btn = Button(sc_frame, text="√", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+sqr_btn.grid(row=1, column=1, padx=4)
 
-pow_btn = Button(sc_frame, text="^", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-pow_btn.grid(row=1, column=0)
-deg_btn = Button(sc_frame, text="ToDeg", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-deg_btn.grid(row=1, column=1)
-rad_btn = Button(sc_frame, text="ToRad", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-rad_btn.grid(row=1, column=2)
-sqr_btn = Button(sc_frame, text="√", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-sqr_btn.grid(row=1, column=3)
-
-
-coma_btn = Button(sc_frame, text=",", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-coma_btn.grid(row=2, column=0)
-log_btn=Button(sc_frame, text="log", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-log_btn.grid(row=2, column=1)
-pi_btn=Button(sc_frame, text="∏", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-pi_btn.grid(row=2, column=2)
-flr_btn=Button(sc_frame, text="└ Floor┘", bg='light blue', activebackground='orange', font="Times 18", width=6, height=2)
-flr_btn.grid(row=2, column=3)
-
+coma_btn = Button(sc_frame, text=",", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+coma_btn.grid(row=1, column=2, padx=4)
+log_btn = Button(sc_frame, text="log", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+log_btn.grid(row=1, column=3, padx=4)
+pi_btn = Button(sc_frame, text="∏", bg='light blue', activebackground='orange', font="Times 12", width=4, height=2)
+pi_btn.grid(row=1, column=4, padx=4)
+flr_btn = Button(sc_frame, text="└ Floor┘", bg='light blue', activebackground='orange', font="Times 12", width=4,
+                 height=2)
+flr_btn.grid(row=1, column=5, padx=4)
 
 # binding all scientific buttons
 sin_btn.bind("<Button-1>", sc_calc)
@@ -282,5 +286,8 @@ deg_btn.bind("<Button-1>", sc_calc)
 rad_btn.bind("<Button-1>", sc_calc)
 sqr_btn.bind("<Button-1>", sc_calc)
 coma_btn.bind("<Button-1>", sc_calc)
+log_btn.bind("<Button-1>", sc_calc)
+pi_btn.bind("<Button-1>", sc_calc)
+flr_btn.bind("<Button-1>", sc_calc)
 
 window.mainloop()
